31af6875e4b00e1f531c91fa46fc0fde1c4b9262
easyfly007 <easyfly.huang@gmail.com>
2017-10-02 21:29:45 +0800

    add the pickle read and write

--- util.py
+++ util.py
@@ -1,20 +1,34 @@
+import pickle
+import os
 from adjmat import AdjTypeMat
 
 def get_inputs_data():
-	adj_obj = AdjTypeMat()
-	features = []
-	with open('caselist.txt', 'r') as f:
-		for file in f.readlines():
-			file = file.strip()
-			adj_obj.readin(file)
-			adj_mat, node_count = adj_obj.buildAdjTypeMat()
-			features.append((adj_mat, node_count))
+	if os.path.isfile('data.pkl', 'r'):
+		with open('data.pkl', 'rb') as f:
+			pickle_data = pickle.load(f)
+			features, labels = pickle_data['features'], pickle_data['labels']
+	else:
+		adj_obj = AdjTypeMat()
+		features = []
+		with open('caselist.txt', 'r') as f:
+			for file in f.readlines():
+				file = file.strip()
+				adj_obj.readin(file)
+				adj_mat, node_count = adj_obj.buildAdjTypeMat()
+				features.append((adj_mat, node_count))
 
-	labels = []
-	with open('labellist.txt', 'r') as f:
-		for line in f.readlines():
-			if line == '1':
-				labels.append(1)
-			else:
-				labels.append(0)
+		labels = []
+		with open('labellist.txt', 'r') as f:
+			for line in f.readlines():
+				if line == '1':
+					labels.append(1)
+				else:
+					labels.append(0)
+
+		with open('dump.pkl', 'wb') as f:
+			pickle.dump({
+				'features': features,
+				'labels': labels},
+				f, pickle.HIGHEST_PROTOCOL)
+
 	return features, labels
