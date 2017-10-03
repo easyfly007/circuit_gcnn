853e2253785b239370aed9bdb0f3aef7dcd56942
easyfly007 <easyfly.huang@gmail.com>
2017-10-03 10:00:32 +0800

    add the unit test for input data build

--- util.py
+++ util.py
@@ -7,6 +7,7 @@ def get_inputs_data():
 		with open('data.pkl', 'rb') as f:
 			pickle_data = pickle.load(f)
 			features, labels = pickle_data['features'], pickle_data['labels']
+			print('get input data from previous saved pickle file')
 	else:
 		adj_obj = AdjTypeMat()
 		features = []
@@ -20,6 +21,7 @@ def get_inputs_data():
 		labels = []
 		with open('labellist.txt', 'r') as f:
 			for line in f.readlines():
+				line = line.strip()
 				if line == '1':
 					labels.append(1.0)
 				else:
@@ -30,8 +32,19 @@ def get_inputs_data():
 				'features': features,
 				'labels': labels},
 				f, pickle.HIGHEST_PROTOCOL)
-	print(type(labels[0]))
 	return features, labels
 
+
 if __name__ == '__main__':
+	# do quick overview of the samples
 	features, labels = get_inputs_data()
+	assert len(features) == len(labels), ' feature/label number not match'
+	print('total samples: ', len(features))
+	
+	sample_count_pos = labels.count(1.0)
+	sample_count_neg = labels.count(0.0)
+	print('positive samples: ', sample_count_pos, 'negative samples: ', sample_count_neg)
+	
+	node_counts = [feature[1] for feature in features]
+	print('the max node count: ', max(node_counts))
+	print('the min node count: ', min(node_counts))
