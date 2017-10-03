92a4d3fea43386b797637d07dad2d7809f039808
easyfly007 <easyfly.huang@gmail.com>
2017-10-03 16:57:38 +0800

    add the test flow codes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from util import get_inputs_data
 
 
 # 1. prepare the input data,
-features, labels = get_inputs_data()
+features_train, labels_train = get_inputs_data('train')
 
 
 # 2. build the network
@@ -20,7 +20,6 @@ logits = network.buildNet(placeholder_node_count, placeholder_adj_mats)
 prob = tf.sigmoid(logits)
 
 cost = -placeholder_label * tf.log(prob) - (1.0- placeholder_label)*tf.log(1.0 - prob)
-# cost = tf.nn.softmax_cross_entropy_with_logits( logits = logits, labels = placeholder_label)
 pred = tf.case(
 	{tf.greater(prob, 0.5): lambda: tf.constant(1.0)}, 
 	default = lambda:tf.constant(0.0))
@@ -28,24 +27,24 @@ accu = tf.case(
 	{tf.equal(pred, placeholder_label): lambda: tf.constant(1.0)}, 
 	default = lambda: tf.constant(0.0))
 
-
 optimizer = tf.train.AdamOptimizer(learning_rate = placeholder_learning_rate).minimize(cost)
-# optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 
 # 3. train
 # set super parameters
-epoches = 20
+epoches = 100
 learning_rate = 0.001
-epoch_print = 5
+epoch_print = 10
 verbose = True
 
 init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
 with tf.Session() as sess:
 	sess.run(init)
 	for epoch in range(epoches):
 		total_accuracy = 0.0
-		for sample_idx, (feature, label) in enumerate(zip(features, labels)):
+		for sample_idx, (feature, label) in enumerate(zip(features_train, labels_train)):
 			adj_mat, node_count = feature
 			feed = {
 				placeholder_learning_rate: learning_rate,
@@ -55,14 +54,37 @@ with tf.Session() as sess:
 			_, probability, accuracy = sess.run((optimizer, prob, accu), feed_dict = feed)
 			total_accuracy += accuracy
 			if verbose and epoch % epoch_print == 0:
-				print('\n\tsample:', sample_idx, 'label is:', label)
+				print('\n\ttrain sample:', sample_idx, 'label is:', label)
 				print('\tthe predicted probability is:', probability)
 				print('\tthe accuracy is:', accuracy)
 		if epoch % epoch_print == 0:
-			total_accuracy /= len(features)
+			total_accuracy /= len(features_train)
 			print('\nEpoch', epoch)
 			print('total accuracy is:', total_accuracy)
 			print('='*60)
+	saver.save(sess, 'saved_models/gcn_model.ckpt')
+	print('trained model saved at \'saved_models/gcn_model.ckpt\'')
 
 
 # 4. test
+features_test, labels_test = get_inputs_data('test')
+with tf.Session() as sess:
+	saver.restore(sess, 'saved_models/gcn_model.ckpt')
+	print('reload model from \'saved_models/gcn_model.ckpt\'')
+
+	total_accuracy = 0.0
+	for sample_idx, (feature, label) in enumerate(zip(features_test, labels_test)):
+		adj_mat, node_count = feature
+
+		feed = {
+			placeholder_adj_mats: adj_mat, 
+			placeholder_node_count: node_count,
+			placeholder_label: label }
+		probability, accuracy = sess.run((prob, accu), feed_dict = feed)
+		total_accuracy += accuracy
+		if verbose:
+			print('\n\ttest sample:', sample_idx, 'label is:', label)
+			print('\tthe predicted probability is:', probability)
+			print('\tthe accuracy is:', accuracy)
+	total_accuracy /= len(features_test)
+	print('total test accuracy is:', total_accuracy)
